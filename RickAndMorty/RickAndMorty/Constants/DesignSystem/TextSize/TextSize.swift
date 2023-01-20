deec0bab30553adcc2bcceb34dafbe3b2a58247f
Angela Alves <angelavbalves@rede.ulbra.br>
2023-01-20 10:33:11 -0300

    feat: define design system

---
+++ RickAndMorty/RickAndMorty/Constants/DesignSystem/TextSize/TextSize.swift
@@ -0,0 +1,14 @@
+//
+//  TextSize.swift
+//  RickAndMorty
+//
+//  Created by Angela Alves on 27/12/22.
+//
+
+import Foundation
+
+struct TextSize {
+    static let small = 12.0
+    static let medium = 16.0
+    static let large = 20.0
+}
