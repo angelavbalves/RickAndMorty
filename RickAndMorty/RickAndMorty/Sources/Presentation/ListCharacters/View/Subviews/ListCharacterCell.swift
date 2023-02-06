f94c72091f2bef57d74d047969afa8ba2aa6ad3a
Angela Alves <angelavbalves@rede.ulbra.br>
2023-02-06 10:37:12 -0300

    feat: list characters view and cell

---
+++ RickAndMorty/RickAndMorty/Sources/Presentation/ListCharacters/View/Subviews/ListCharacterCell.swift
@@ -0,0 +1,16 @@
+//
+//  ListCharacterCell.swift
+//  RickAndMorty
+//
+//  Created by Angela Alves on 20/01/23.
+//
+
+import Foundation
+import UIKit
+import TinyConstraints
+import Kingfisher
+
+class ListCharacterCell: UITableViewCell {
+
+    static let identifier = "characterCell"
+}
