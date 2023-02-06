1e08f2bacc80e50e7887f9c176d9cd7172d600c0
Angela Alves <angelavbalves@rede.ulbra.br>
2023-02-06 10:32:41 -0300

    feat: ListCharactersCoordinator

---
+++ RickAndMorty/RickAndMorty/Sources/Presentation/ListCharacters/Coordinator/ListCharactersCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  ListCharactersCoordinator.swift
+//  RickAndMorty
+//
+//  Created by Angela Alves on 20/01/23.
+//
+
+import Foundation
+import UIKit
+
+class ListCharactersCoordinator: CoordinatorProtocol {
+
+    // MARK: - Properties
+    var rootViewController: RMNavigationController?
+    var childCoordinator: [CoordinatorProtocol] = []
+
+    // MARK: - Properties
+    init(rootViewController: RMNavigationController = .init()) {
+        self.rootViewController = rootViewController
+    }
+    
+    func start() {
+        let viewModel = ListCharactersViewModel(coordinator: self)
+        let controller = ListCharactersViewController(viewModel: viewModel)
+
+        rootViewController?.setViewControllers([controller], animated: false)
+    }
+}
