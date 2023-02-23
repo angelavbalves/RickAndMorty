e06fb6a0251f87296ed041c4e3268f1f3fff3e81
Angela Alves <angelavbalves@rede.ulbra.br>
2023-02-23 14:15:52 -0300

    feat: include init from response

--- RickAndMorty/RickAndMorty/Sources/Service/Models/EpisodesResponse.swift
+++ RickAndMorty/RickAndMorty/Sources/Service/Models/EpisodesResponse.swift
@@ -8,7 +8,14 @@
 import Foundation
 
 struct EpisodesResponse: Codable, Equatable {
-    let results: [EpisodeResponse]
+    let results: [EpisodeResponseItem]
+}
+
+struct EpisodeResponseItem: Codable, Equatable {
+    let id: Int
+    let name: String
+    let episode: String
+    let characters: [URL]
 }
 
 struct EpisodeResponse: Codable, Equatable {
@@ -16,4 +23,11 @@ struct EpisodeResponse: Codable, Equatable {
     let name: String
     let episode: String
     let characters: [URL]
+
+    init(from response: EpisodeResponseItem) {
+        self.id = response.id
+        self.name = response.name
+        self.episode = response.episode
+        self.characters = response.characters
+    }
 }
