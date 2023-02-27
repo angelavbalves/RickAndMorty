01c5364300f2bdba25b3d45dc956d12df1464ea4
Angela Alves <angelavbalves@rede.ulbra.br>
2023-02-27 16:07:42 -0300

    feat: request in the view controller

--- RickAndMorty/RickAndMorty/Sources/Presentation/ListPlaces/Controller/ListPlacesViewController.swift
+++ RickAndMorty/RickAndMorty/Sources/Presentation/ListPlaces/Controller/ListPlacesViewController.swift
@@ -7,11 +7,15 @@
 
 import Foundation
 import UIKit
+import RxSwift
 
 class ListPlacesViewController: RMViewController {
 
     // MARK: - Properties
     private let viewModel: ListPlacesViewModel
+    private var places = [LocationResponse]()
+    private let disposeBag = DisposeBag()
+    private var currentPage = 1
     private lazy var barButtonItem = UIBarButtonItem(
         image: UIImage(systemName: "text.justify"),
         style: .done,
@@ -34,7 +38,24 @@ class ListPlacesViewController: RMViewController {
     }
 
     override func viewDidLoad() {
-        view.backgroundColor = .systemPink
+        super.viewDidLoad()
+        title = "Places"
+        view.backgroundColor = AppColors.lightPink
         navigationItem.leftBarButtonItem = barButtonItem
+        fetchPlaces(currentPage, "")
+    }
+
+    private func fetchPlaces(_ currentPage: Int, _ query: String) {
+        guard let url = ApiClient.getURLs()?.locations else { return }
+        viewModel
+            .fetchPlaces(url, currentPage, query)
+            .subscribe(onNext: { [weak self] locationsResponse in
+                let places = locationsResponse.results.map(LocationResponse.init)
+                self?.places = places
+                DispatchQueue.main.async {
+                    self?.listPlacesView.receive(places)
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
