0b36461bbcb55631bbc350a96610aeaff14d84c2
Angela Alves <angelavbalves@rede.ulbra.br>
2023-04-16 15:07:26 -0300

    feat: add route to episode coordinator

---
+++ RickAndMorty/RickAndMorty/Sources/Presentation/Coordinator/AppCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  AppCoordinator.swift
+//  RickAndMorty
+//
+//  Created by Angela Alves on 20/01/23.
+//
+
+import RxSwift
+import UIKit
+import XCoordinator
+
+class AppCoordinator: NavigationCoordinator<AppRoutes> {
+
+    init() {
+        super.init(rootViewController: UINavigationController(), initialRoute: .home)
+    }
+
+    override func prepareTransition(for route: AppRoutes) -> NavigationTransition {
+        switch route {
+            case .home:
+                let homeViewController = HomeViewController(router: weakRouter)
+                return .push(homeViewController)
+            case .characters:
+                let coordinator = ListCharacterCoordinator()
+                return .present(coordinator)
+            case .locations:
+                return .none()
+            case .episodes:
+                let coordinator = ListEpisodeCoordinator()
+                return .present(coordinator)
+        }
+    }
+}
