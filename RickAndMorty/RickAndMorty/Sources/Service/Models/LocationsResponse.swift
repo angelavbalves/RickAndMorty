0b584970844d49e8d3b3f9b2a6f23986bbe31c55
Angela Alves <angelavbalves@rede.ulbra.br>
2023-04-16 15:54:19 -0300

    refactor: remove init from response

--- RickAndMorty/RickAndMorty/Sources/Service/Models/LocationsResponse.swift
+++ RickAndMorty/RickAndMorty/Sources/Service/Models/LocationsResponse.swift
@@ -18,19 +18,3 @@ struct LocationResponseItem: Codable, Equatable {
     let dimension: String
     let residents: [URL]
 }
-
-struct LocationResponse: Codable, Equatable {
-    let id: Int
-    let name: String
-    let type: String
-    let dimension: String
-    let residents: [URL]
-
-    init(from response: LocationResponseItem) {
-        self.id = response.id
-        self.name = response.name
-        self.type = response.type
-        self.dimension = response.dimension
-        self.residents = response.residents
-    }
-}
